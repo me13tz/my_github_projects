063ae8062512910523f5003da0748045f409a3cb
me13tz <me13tz@yahoo.com>
2015-05-29 23:42:42 -0700

    string methods
    a study of different methods, updated.

--- string_fun.py
+++ string_fun.py
@@ -1,14 +1,20 @@
 import sys
+import random
 
-d = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'nerd']
-x = 0
+
+s = "The quick brown fox jumps over the lazy nerd."
+x = (s.lower())  ###place string in lowercase
+rep = x.replace('.', '')  ###remove periods
+d = list(rep.split(' '))  ###create list splitting words by white spaces
+print(d)
+x = len(d)
 w = []
 l = []
 for word in d:
-    c = d[x]
+    c = d[x - 1]
     print(c)
-    x+=1
-print('...')
+    x -= 1  ###backwards and forwards...
+print('........................................................................')
 for i in range(len(d)):
     a = len(d)
     a -= 1
@@ -17,7 +23,7 @@ for i in range(len(d)):
     d.pop()
     a -= 1
 print(w)
-print('...')
+print('........................................................................')
 m = []
 for i in range(len(w)):
     b = len(w)
@@ -26,32 +32,34 @@ for i in range(len(w)):
     m.append(l)
     w.pop()
     b -= 1
-print (m)
-print('...')
+print(m)
+print('........................................................................')
 u = []
 b = len(m)
 for word in m:
     b -= 1
     u.append(m[b])
-print()
+
 print(u)
-print()
+print('........................................................................')
 print(m)
-print()
-str1=' '.join(m)
-str2='.'.join(m)
-str3='*'.join(m)
-str4=''.join(m)
+print('........................................................................')
+str1 = ' '.join(m)  ###join list in a string using spaces
+str2 = '.'.join(m) ###join list in a string using periods
+str3 = '*'.join(m) ###join list in a string using stars
+str4 = ''.join(m)  ###join list using nothing
 print()
 print(str1)
 print(str2)
 print(str3)
 print(str4)
-print()
-if 'heaven' in str2:
-    print('Found it!')
-print()
-print(m)
-for word in m:
-    sys.stdout.write(word[-1]) ###prints it all on the same line, from a list
+print('........................................................................')
+b = random.sample(m, len(m)) ###randomize the contents of the list
+print(b)
+random.shuffle(b) ###randomize again
+print(b)
+b.reverse()  ### reverse the mess
+print(b)
 
+for word in b:
+    sys.stdout.write(word[-1]) ###prints last letter of randomized list elements on the same line
