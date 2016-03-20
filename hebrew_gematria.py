b9ea99b8428710e7821a64eae398db54ae433018
Josh Metz <me13tz@yahoo.com>
2016-03-20 13:03:56 -0700

    update Path and make notes

--- hebrew_gematria.py
+++ hebrew_gematria.py
@@ -4,7 +4,7 @@ Score = {'א': 1, 'ב': 2, 'ג': 3, 'ד': 4, 'ה': 5, 'ו': 6, 'ז': 7, 'ח': 8,
          'ל': 30, 'מ': 40, 'נ': 50, 'ס': 60, 'ע': 70, 'פ': 80, 'צ': 90, 'ק': 100, 'ר': 200,
          'ש': 300, 'ת': 400, 'ך': 100, 'ם': 40, 'ן': 50,  'ף': 80, 'ץ': 90}
 
-interesting_nums = [61, 26, 55]
+interesting_nums = [61, 26, 55]  ## add your own numbers to the list, script will highlight these in the output
 
 def decompose(num):
     sum = num
@@ -15,7 +15,8 @@ def decompose(num):
         sum = new_sum
     return sum
 
-importfile = open(r'/Users/joshmetz/Documents/Pycharm_projects/feed.txt')  # changed this so it will work on my local machine
+importfile = open(r'/Users/Path/To/feed.txt')  # change this so will work on your machine
+
 text = str(importfile.read())
 importfile.close()
 
